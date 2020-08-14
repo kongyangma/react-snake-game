b928ce5649daddfb2c50152d702863de4155d7eb
kongyangma <kongyangma@hotmail.com>
2020-08-14 16:47:10 -0700

    first time upload files

---
+++ src/component/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Portal from '../../common/Portal';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Portal />
+      </div>
+    );
+  }
+}
+
+export default App;
