8448f83505c2186a6df439ac80f3a5d9905cd739
kongyangma <kongyangma@hotmail.com>
2020-08-14 17:52:47 -0700

    Updates

---
+++ precache-manifest.e38b80c5c824cd331ab717774063c352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a990b4a02d6347fbb1ba322d9b6f5a2b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c8656a4ceeafa5879028",
+    "url": "./static/css/main.1af092c0.chunk.css"
+  },
+  {
+    "revision": "a7de3e12ffe8edc62acd",
+    "url": "./static/js/2.99ce42be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.99ce42be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8656a4ceeafa5879028",
+    "url": "./static/js/main.e4e57749.chunk.js"
+  },
+  {
+    "revision": "0d94dbc03668d9eadce9",
+    "url": "./static/js/runtime-main.a173f218.js"
+  }
+]);
